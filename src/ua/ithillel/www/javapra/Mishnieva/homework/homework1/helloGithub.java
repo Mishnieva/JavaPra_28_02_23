eb0bc5ea71a2e49614c11217f8e4092f7f622b12
Mishnieva Julia <juli8md@gmal.com>
2023-03-02 17:51:35 +0100

    helloGithub

--- src/ua/ithillel/www/javapra/Mishnieva/homework/homework1/helloGithub.java
+++ src/ua/ithillel/www/javapra/Mishnieva/homework/homework1/helloGithub.java
@@ -2,6 +2,6 @@ package ua.ithillel.www.javapra.Mishnieva.homework.homework1;
 
 public class helloGithub {
     public static void main(String[] args) {
-        System.out.println("Hello Github!");
+        System.out.println("Hello Github!!!");
     }
 }
